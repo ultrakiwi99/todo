368e1db17e54ea23021aa48cab07bc4ce3d01d12
aleksandrkruzhkov <alex.krz.1980@gmail.com>
2022-08-04 20:42:03 +0300

    Ui working sketch.

--- src/ui/todos/TodoList.tsx
+++ src/ui/todos/TodoList.tsx
@@ -2,15 +2,26 @@ import {useTodoCases} from "../../application/useTodoCases";
 import {MemoryTodoRepository} from "../../application/todoRepository";
 
 export function TodoList() {
-  const {todos} = useTodoCases(new MemoryTodoRepository());
+  const {
+    todos,
+    addTodoCase,
+    removeTodoCase,
+    toggleDoneCase
+  } = useTodoCases(new MemoryTodoRepository());
 
   return (
     <section>
       <h1>List of Todos</h1>
       <ul>
-        {todos.map(todo => <li>{todo.title}: {todo.completed}</li>)}
+        {todos.map(todo => <li>
+          <span style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
+            {todo.title}
+          </span>
+          <button onClick={() => toggleDoneCase(todo)}>V</button>
+          <button onClick={() => removeTodoCase(todo)}>X</button>
+        </li>)}
       </ul>
-      <button>Add todo</button>
+      <button onClick={() => addTodoCase(MemoryTodoRepository.randomTodo())}>Add todo</button>
     </section>
   )
 }
